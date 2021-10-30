7e1398591ae13633f99aeb42f529ef0c94e1ade7
kong <duguyihou@gmail.com>
2021-10-30 21:46:58 +1100

    chore: remove avatar in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{css,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      avatar: 'url(https://avatars.githubusercontent.com/u/9347790?v=4)',
-    },
+    extend: {},
   },
   variants: {
     extend: {},
