09230d8a07df7a23718b7302228b9efeb4bb268b
kong <duguyihou@gmail.com>
2021-10-30 14:35:39 +1100

    feat(css): add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import react from '@vitejs/plugin-react'
-import autoprefixer from 'autoprefixer'
 import path from 'path'
 import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: { plugins: [autoprefixer()] },
-  },
+
   resolve: {
     alias: [
       { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
