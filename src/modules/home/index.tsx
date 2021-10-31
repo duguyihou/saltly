0efc021d61e9ad17152b0b1e41552c85586effe7
kong <duguyihou@gmail.com>
2021-10-31 21:42:34 +1100

    feat: add react-redux reduxToolkit

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,12 +1,16 @@
+import { useAppDispatch, useAppSelector } from '@app/hooks'
+import { fetchPost, postsSelector } from '@app/postSlice'
 import Feed from '@cmpts/feed'
 import Layout from '@cmpts/layout'
+import { useEffect } from 'react'
 
 function Home() {
-  return (
-    <Layout>
-      <Feed />
-    </Layout>
-  )
+  const dispatch = useAppDispatch()
+  const { posts } = useAppSelector(postsSelector)
+  useEffect(() => {
+    dispatch(fetchPost())
+  }, [])
+  return <Layout>{posts && <Feed posts={posts} />}</Layout>
 }
 
 export default Home
