d6fe610aa13f3d17e8477e130ed26eefe1a62fe4
kong <duguyihou@gmail.com>
2021-11-01 07:53:58 +1100

    refactor: remove three js

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -1,19 +1,5 @@
-import { Canvas } from '@react-three/fiber'
-
-import Box from './Box'
-
 function Background() {
-  return (
-    <div className="w-full h-full absolute bg-gray-500">
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <Box position={[-3, 0, 0]} />
-        <Box position={[3, 0, 0]} />
-      </Canvas>
-    </div>
-  )
+  return <div className="w-full h-full absolute bg-gray-500" />
 }
 
 export default Background
